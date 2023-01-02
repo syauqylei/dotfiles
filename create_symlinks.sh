9514cf6f446422ab5a01e1cc0783931a51d82649
syauqy <syauqilenterano@gmail.com>
2023-01-02 09:14:09 +0700

    add tmux conf

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#neovim
 echo "copying configs  to $HOME/.config/"
-cp -as $PWD/nvim $HOME/.config/
+#cp -as $PWD/nvim $HOME/.config/
 cp -as $PWD/kitty $HOME/.config/
 cp $PWD/.tmux.conf $HOME
